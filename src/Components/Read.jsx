6a7dbc14aced93a10511adc11df2a326229ebb0b
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-09-04 22:02:02 -0700

    Create Read.jsx

---
+++ src/Components/Read.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+
+function Read() {
+  const [list, setList]=useState([])
+  const {id}=useParams();
+
+  useEffect(()=>{
+    axios.get(`http://localhost:3000/user/${id}`)
+    .then(response=>{
+      setList(response.data);
+    })
+    .catch(err=>console.log(err));
+  },[])
+
+  return (
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
+      <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+        <h3>Detail of User</h3>
+        <div className='mb-2'>
+          <strong>Name: {list.name}</strong>
+        </div>
+        <div className='mb-2'>
+          <strong>Email: {list.email}</strong>
+        </div>
+        <div className='mb-3'>
+          <strong>Phone: {list.phone}</strong>
+        </div>
+        <Link to={`/update/${id}`} className="btn btn-success">Edit</Link>
+        <Link to={'/'} className='btn btn-primary ms-3'>Back</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Read
